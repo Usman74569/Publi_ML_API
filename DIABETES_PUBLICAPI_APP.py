9ac07ee88601021a2f05c5b8a8ca9d4ba0d6f3f8
Usman74569 <usmansyed972@gmail.com>
2025-06-30 17:45:03 +0500

    Add files via upload

---
+++ DIABETES_PUBLICAPI_APP.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 30 12:19:45 2025
+
+@author: DELL
+"""
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+import numpy as np
+import pickle
+
+app = FastAPI()
+
+# Add CORS middleware
+origins = ["*"]  # allow all origins (you can restrict later)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+# Load model and scaler
+model = pickle.load(open("diabetes_model.sav", "rb"))
+scaler = pickle.load(open("diabetes_scaler.sav", "rb"))
+
+# Input schema
+class DiabetesFeatures(BaseModel):
+    Pregnancies: float
+    Glucose: float
+    BloodPressure: float
+    SkinThickness: float
+    Insulin: float
+    BMI: float
+    DiabetesPedigreeFunction: float
+    Age: float
+
+@app.post("/predict")
+def predict_diabetes(data: DiabetesFeatures):
+    input_data = np.array([[data.Pregnancies, data.Glucose, data.BloodPressure,
+                            data.SkinThickness, data.Insulin, data.BMI,
+                            data.DiabetesPedigreeFunction, data.Age]])
+    input_scaled = scaler.transform(input_data)
+    prediction = model.predict(input_scaled)
+    return {"prediction": "Diabetic" if prediction[0] == 1 else "Non-Diabetic"}
